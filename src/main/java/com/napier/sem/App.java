d066663c6d223e33300e428305d95bdaf8a6e8bd
Shine Lin Ko <40501827@napier.ac.uk>
2022-03-20 11:38:29 +0630

    Add new database url for git workflow

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -23,7 +23,6 @@ public class App
      * Connection to MySQL database.
      */
     private Connection con = null;
-
     /**
      * Connect to the MySQL database.
      */
@@ -40,10 +39,34 @@ public class App
             System.exit(-1);
         }
 
+        int bretries = 10;
+        for (int i = 0; i < bretries; ++i)
+        {
+            System.out.println("Type 1:Connecting to database...");
+            try
+            {
+                // Wait a bit for db to start
+                Thread.sleep(30000);
+                // Connect to database
+                con = DriverManager.getConnection("jdbc:mysql://db:3306/employees", "root", "example");
+                System.out.println("Successfully connected");
+                break;
+            }
+            catch (SQLException sqle)
+            {
+                System.out.println("Failed to connect to database attempt " + i);
+                System.out.println(sqle.getMessage());
+            }
+            catch (InterruptedException ie)
+            {
+                System.out.println("Thread interrupted? Should not happen.");
+            }
+        }
+
         int retries = 10;
         for (int i = 0; i < retries; ++i)
         {
-            System.out.println("Connecting to database...");
+            System.out.println("Type 2:Connecting to database...");
             try
             {
                 // Wait a bit for db to start
@@ -55,7 +78,7 @@ public class App
             }
             catch (SQLException sqle)
             {
-                System.out.println("Failed to connect to database attempt " + Integer.toString(i));
+                System.out.println("Failed to connect to database attempt " + i);
                 System.out.println(sqle.getMessage());
             }
             catch (InterruptedException ie)
